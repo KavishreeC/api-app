35a7fd45a774faf37e0f48df94fc4eb99e115633
KavishreeC <kavishree.c@tringapps.com>
2021-04-19 11:03:02 +0530

    added client

--- app/commands/authorize_api_request.rb
+++ app/commands/authorize_api_request.rb
@@ -14,7 +14,7 @@ class AuthorizeApiRequest
 
   attr_reader :headers
 
-  def user
+  def client
     @client ||= Client.find(decoded_auth_token[:user_id]) if decoded_auth_token
     @client || errors.add(:token, 'Invalid token') && nil
   end
