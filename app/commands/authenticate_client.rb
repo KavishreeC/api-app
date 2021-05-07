a7ed150b37db5f8dd30cc73f9deef3f50892e997
KavishreeC <kavishree.c@tringapps.com>
2021-05-07 10:46:35 +0530

    fixed indentation

--- app/commands/authenticate_client.rb
+++ app/commands/authenticate_client.rb
@@ -1,25 +1,23 @@
-
 class AuthenticateClient
-    prepend SimpleCommand
+  prepend SimpleCommand
   
-    def initialize(email, password)
-      @email = email
-      @password = password
-    end
-  
-    def call
-      JsonWebToken.encode(client_id: client.id) if client
-    end
-  
-    private
-  
-    attr_accessor :email, :password
-  
-    def client
-      client = Client.find_by_email(email)
-      return client if client && client.authenticate(password)
-  
-      errors.add :client_authentication, 'invalid credentials'
-      nil
-    end
+  def initialize(email, password)
+    @email = email
+    @password = password
   end
+  
+  def call
+    JsonWebToken.encode(client_id: client.id) if client
+  end
+  
+  private
+  
+  attr_accessor :email, :password
+  
+  def client
+    client = Client.find_by_email(email)
+    return client if client && client.authenticate(password)
+    errors.add :client_authentication, 'invalid credentials'
+    nil
+  end
+end
